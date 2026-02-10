33857a0adc4a5589d7255f35e8c6d88c910519d7
Posixoxo <komo@example.com>
2026-02-10 10:08:49 -0800

    modified styles

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'; // This is what's missing!
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', //
+  base: '/', // Change this from './' to '/'
 })
